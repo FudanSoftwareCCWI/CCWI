c566bc873d75ef0a955d8062a39e8dab71aa0b7e
SidneyFanFan <SidneyFan716@gmail.com>
2014-03-06 16:13:00 +0800

    app

---
+++ app/controllers/Pub.php
@@ -0,0 +1,29 @@
+<?php
+class Pub{
+	
+	public static function pub_sidebar($path){
+		$activities = opendir($path);
+		$i=0;
+		while (($file = readdir($activities)) !== false) { 
+			if(pathinfo($file, PATHINFO_EXTENSION)=='html'){
+				printf(PubModel::make_li($i+1,pathinfo($file, PATHINFO_FILENAME)));
+				$i++;
+			}
+		}
+		closedir($activities); 
+	}
+	
+	public static function pub_content($path){
+		$activities = opendir($path);
+		$i=0;
+		while (($file = readdir($activities)) !== false) { 
+			if(pathinfo($file, PATHINFO_EXTENSION)=='html'){
+				echo(PubModel::make_content($i+1,substr(pathinfo($file, PATHINFO_FILENAME),12),substr($file,0,10),$path.$file));
+				$i++;
+			}
+		}
+		closedir($activities); 
+	}
+	
+		
+}
---
+++ app/models/PubModel.php
@@ -0,0 +1,18 @@
+<?php
+class PubModel{
+
+	public static function make_li($index,$name){
+		return sprintf("<li class='default-li' id='1-%d'>%s</li>",$index,$name);
+	}
+	
+	public static function make_content($index,$name,$time,$iframe){
+		$ret = "";
+		$ret = $ret.sprintf("<div class='item_content showpart' id='content-1-%d'>",$index);
+		$ret = $ret.sprintf("<h3>活动简介</h3><p>名称:%s</p><p>时间:%s</p>",$name,$time);
+		$ret = $ret.sprintf("<iframe src=%s",$iframe)." frameborder='0' id='frame_con' class='cke_wysiwyg_frame cke_reset' style='width:100%;height:500px;background-color:rgb(247, 247, 247);' scrolling=no></iframe>";
+		$ret = $ret."<script type='text/javascript'>$('#frame_con').load(function(){var thisheight = $(this).contents().find('body').height()+30;$(this).height(thisheight < 500 ? 500 : thisheight);});</script></div>";
+		return $ret;
+			
+	}
+		
+}
